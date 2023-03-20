e8abf009c55a86142bc385d10b57b3e3e869de63
«Kristina» <kristinka007_92@mail.ru>
2023-03-20 15:25:30 +0300

    Задача 20

--- Dz_Tasks.py
+++ Dz_Tasks.py
@@ -8,14 +8,53 @@
 #     3
 #     -> 1
 
+# n = int(input("Введите количество элементов массива: "))
+# import random
+# my_list = [random.randint(1,10) for i in range(n)]
+# print (my_list)
 
-import random
-my_list = [random.randint(1,10) for i in range(5)]
-print (my_list)
+# X = int (input ('Введите число: '))
+# count = 0
+# for i in my_list:
+#     if i == X:
+#         count += 1
+# print(f"{X} встречается {count} раз(а) ")
 
-X = int (input ('Введите число: '))
+
+# Задача 20: * В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность. В случае с английским алфавитом очки 
+# распределяются так:A, E, I, O, U, L, N, S, T, R – 1 очко; D, G – 2 очка; B, C, M, P – 3 очка; F, H, V, W, Y – 4 очка; K – 5 очков; J, X – 8 очков; 
+# Q, Z – 10 очков. А русские буквы оцениваются так: А, В, Е, И, Н, О, Р, С, Т – 1 очко; Д, К, Л, М, П, У – 2 очка; Б, Г, Ё, Ь, Я – 3 очка; 
+# Й, Ы – 4 очка; Ж, З, Х, Ц, Ч – 5 очков; Ш, Э, Ю – 8 очков; Ф, Щ, Ъ – 10 очков. 
+# Напишите программу, которая вычисляет стоимость введенного пользователем слова. 
+# Будем считать, что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.
+
+# *Пример:*
+
+# ноутбук
+#     12
+
+dictionary = {
+        1: "A,E,I,O,U,L,N,S,T,R,А,В,Е,И,Н,О,Р,С,Т".split(","),
+        2: "D,G,Д,К,Л,М,П,У".split(","),
+        3: "B,C,M,P,Б,Г,Ё,Ь,Я".split(","),
+        4: "F,H,V,W,Y,Й,Ы".split(","),
+        5: "K,Ж,З,Х,Ц,Ч".split(","),
+        8: "J,X,Ш,Э,Ю".split(","),
+        10: "Q,Z,Ф,Щ,Ъ".split(","), }
+word = input("Введите слово: ").upper()
+
 count = 0
-for i in my_list:
-    if i == X:
-        count += 1
-print(f"{X} встречается {count} раз(а) ")
+# point = 0
+# for letter in word:
+#     for item in dictionary.items():
+#         if letter in item[1]:
+#             points += item[0]
+# print(word)
+# print(points)
+for i in word:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNMАВЕИНОРСТДКЛМПУБГЁЬЯЙЫЖЗХЦЧШЭЮФЩЪ':
+        for j in dictionary:
+            if i in dictionary[j]:
+                count += j
+
+print(f'Стоимость слова: {count}')
